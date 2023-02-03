599d271ef906d7dc2755f93e73a9c22b5624ea6b
delapzzz <112954170+delapzzz@users.noreply.github.com>
2023-02-03 11:43:39 +0700

    current update

---
+++ Work_2/JS/script.js
@@ -0,0 +1,11 @@
+var me = {
+    fname: "Chewin",
+    lname: "Grerasitsirt",
+    Jtitle: "Student",
+    Tel: "062-292-9994",
+    Email:"chewintar@gmail.com"
+}
+for(var x in me){
+    console.log(me[x]);
+}
+console.log(me);
