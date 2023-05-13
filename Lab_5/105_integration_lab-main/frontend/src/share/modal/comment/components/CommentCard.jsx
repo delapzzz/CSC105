e1c753bcc238c2d83830913c13761e99925a3806
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-05-14 01:58:59 +0700

    update

--- Lab_5/105_integration_lab-main/frontend/src/share/modal/comment/components/CommentCard.jsx
+++ Lab_5/105_integration_lab-main/frontend/src/share/modal/comment/components/CommentCard.jsx
@@ -1,18 +1,29 @@
 import { Button, Card, TextField, Typography } from '@mui/material';
-import React, { useCallback, useState } from 'react';
-import Cookies from 'js-cookie';
-import Axios from '../../../AxiosInstance';
+import React, { useCallback, useState, useContext, useEffect } from 'react';
+import GlobalContext from '../../../Context/GlobalContext';
 import { AxiosError } from 'axios';
+import Axios from '../../../AxiosInstance';
+import Cookies from 'js-cookie';
 
 const CommentCard = ({ comment = { id: -1, msg: '' }, setComments = () => { } }) => {
+  const { user, setStatus } = useContext(GlobalContext);
   const [isConfirm, setIsConfirm] = useState(false);
   const [functionMode, setFunctionMode] = useState('update');
   const [msg, setMsg] = useState(comment.msg);
+  const [msgError, setMsgError] = useState('');
 
-  const submit = useCallback(async() => {
+  // const [msgs, setMsgs] = useState([]);
+
+  // useEffect(() => {
+  //   setMsg(comment.msg);
+  // }, [comment.msg]);
+
+  const submit = useCallback(async () => {
     if (functionMode === 'update') {
       // TODO implement update logic
+      if (!validateForm()) return;
       try {
+        // 2. call API to update note
         const userToken = Cookies.get('UserToken');
         const response = await Axios.patch(
           '/comment',
@@ -24,52 +35,61 @@ const CommentCard = ({ comment = { id: -1, msg: '' }, setComments = () => { } })
             headers: { Authorization: `Bearer ${userToken}` },
           }
         );
-      
+        // 3. if successful, update note in state and close modal
         if (response.data.success) {
-          // update comment in the parent component
-          comment.msg = response.data.data.text;
-          console.log('update success');
-          cancelAction(); // to toggle off the confirm 
-        } else {
-          console.log('Failed to update comment');
+          setStatus({ severity: 'success', msg: 'Update comment successfully' });
+          setComments((comments) => comments.map((c) => (c.id === comment.id ? { ...c, msg: response.data.data.text } : c)));
+          cancelAction();
         }
       } catch (error) {
+        // 4. if update note failed, check if error is from calling API or not
         if (error instanceof AxiosError && error.response) {
-          console.log(error.response.data);
+          setStatus({ severity: 'error', msg: error.response.data.error });
         } else {
-          console.log(error.message);
+          setStatus({ severity: 'error', msg: error.message });
         }
       }
-              
+      // console.log('update');
     } else if (functionMode === 'delete') {
-      // Send DELETE request to the server
+      // TODO implement delete logic
       try {
+        // 1. call API to delete note
         const userToken = Cookies.get('UserToken');
         const response = await Axios.delete('/comment', {
           headers: { Authorization: `Bearer ${userToken}` },
           data: { commentId: comment.id }
         }
         );
+        // 2. if successful, set status and remove note from state
         if (response.data.success) {
-          // Perform any necessary actions after successful deletion
+          setStatus({ severity: 'success', msg: 'Delete comment successfully' });
           setComments((comments) => comments.filter((c) => c.id !== comment.id));
-          console.log('Comment deleted successfully');
-          cancelAction(); // to toggle off the confirm
-        } else {
-          console.log('Failed to delete comment');
+          cancelAction();
         }
       } catch (error) {
+        // 3. if delete note failed, check if error is from calling API or not
         if (error instanceof AxiosError && error.response) {
-          console.log(error.response.data);
+          setStatus({ severity: 'error', msg: error.response.data.error });
         } else {
-          console.log(error.message);
+          setStatus({ severity: 'error', msg: error.message });
         }
       }
+      // console.log('delete');
     } else {
       // TODO setStatus (snackbar) to error
       console.log('error');
     }
   }, [functionMode, msg]);
+
+  const validateForm = () => {
+    let isValid = true;
+    //check user
+    if (!msg) {
+      setMsgError('Comment is required');
+      isValid = false;
+    }
+    return isValid;
+  }
 
   const changeMode = (mode) => {
     setFunctionMode(mode);
@@ -86,7 +106,11 @@ const CommentCard = ({ comment = { id: -1, msg: '' }, setComments = () => { } })
       {!(isConfirm && functionMode == 'update') ? (
         <Typography sx={{ flex: 1 }}>{comment.msg}</Typography>
       ) : (
-        <TextField sx={{ flex: 1 }} value={msg} onChange={(e) => setMsg(e.target.value)} />
+        <TextField sx={{ flex: 1 }}
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+          error={msgError !== ''}
+          helperText={msgError} />
       )}
       {!isConfirm ? (
         <Button onClick={() => changeMode('update')} variant="outlined" color="info">
